cd37bb544b119ed9f057deac8b96b7eff16250bb
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-05-19 18:42:18 -0400

    Added assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Cat", "Cat");
+assertEqual(5, 7);
+console.log("🐈");
