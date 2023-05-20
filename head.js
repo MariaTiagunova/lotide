9dfed09f69b900e0f465972deac46e550e7d916d
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-05-19 22:01:58 -0400

    Added head.js and tail.js, removed cat emoji

--- head.js
+++ head.js
@@ -1,4 +1,9 @@
 // FUNCTION IMPLEMENTATION
+const head = function(array) {
+  return array[0];
+};
+
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
@@ -8,7 +13,7 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Cat", "Cat");
-assertEqual(5, 7);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([20]), 20);
+assertEqual(head([]), undefined);
