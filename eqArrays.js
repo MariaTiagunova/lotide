765d07b9d62e250de1684d0375d54a2f9e4028bd
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-05-21 14:14:35 -0400

    Added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([14, 13, 5], ['14', '13', '5']), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+assertEqual(eqArrays(['a', 'b', 'c'], ['a', 'b', 'c']), true);
+assertEqual(eqArrays(['a', 'b', 'c'], ['a', 'b']), false);
