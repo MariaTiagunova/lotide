4a1a5db2eb2fb470d32a5507b9a2d4c00e3cb15b
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-05-21 16:31:36 -0400

    Added without.js removing array elements

---
+++ without.js
@@ -0,0 +1,37 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const without = (sourceArr, itemsToRemove) => {
+  let result = [];
+  for (let i = 0; i < sourceArr.length; i++) {
+    if (!itemsToRemove.includes(sourceArr[i])) {
+      result.push(sourceArr[i]);
+    }
+  }
+  return result;
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+const words = ["hello", "world", "lighthouse"];
+assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
+
+
