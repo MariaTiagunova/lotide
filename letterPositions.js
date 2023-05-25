0aafe1eb3b8780a1c6ac91b168407291157ccd00
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-05-24 21:11:52 -0400

    letterPositions.js: added working ligic w/comments

--- letterPositions.js
+++ letterPositions.js
@@ -1,32 +1,39 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
+const eqArrays = (actual, expected) => {
+  if (actual.length !== expected.length) {
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
       return false;
     }
   }
   return true;
 };
 
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Arrays are equal: ${arr1} === ${arr2}`);
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌ Arrays are not equal: ${arr1} !== ${arr2}`);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 
 const letterPositions = function(sentence) {
-  const results = {};
+  const result = {};
   // logic to update results here
-  for(let i = 0; i < sentence.length; i++) {
-    
+  for (let i = 0; i < sentence.length; i++) { // iterating over the sentence
+    if (result[sentence[i]]) { // checking if the letter (sentence[i]) already exists as a key in the result object
+      result[sentence[i]].push(i); // adding the current index (i) to the existing array of indices for that letter
+    } else {
+      result[sentence[i]] = [i]; // creating a new key-value pair, with the letter as the key and an array containing the current index (i) as the value
+    }
   }
-  return results;
+  // console.log(result); // printing the result object for testing/verification
+  return result;
 };
 
 //Test
 assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("lighthouse").h, [3, 5]);
