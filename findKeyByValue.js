d6d49551bb1073286baf31a587bc8703d91f6e9c
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-05-24 21:56:50 -0400

    findKeyByValue: added more tests

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -23,3 +23,14 @@ const bestTVShowsByGenre = {
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const tvShows = {
+  drama: 'Breaking Bad',
+  comedy: 'Friends',
+  sciFi: 'Stranger Things',
+  thriller: 'Black Mirror'
+};
+
+assertEqual(findKeyByValue(tvShows, 'Friends'), 'comedy');
+assertEqual(findKeyByValue(tvShows, 'Breaking Bad'), 'drama');
+assertEqual(findKeyByValue(tvShows, 'Game of Thrones'), undefined);
