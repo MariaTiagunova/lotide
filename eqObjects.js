999c60ec899f95bdc73b5424c6d1bca1b22d4705
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-05-25 14:00:04 -0400

    eqObjects: added comparison of primitives as values

--- eqObjects.js
+++ eqObjects.js
@@ -9,4 +9,25 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1); //key arrays
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) { //checks if key arrays have the same length (objects have the same number of keys)
+    return false;
+  }
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
 };
+
+//Test
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+// eqObjects(shirtObject , anotherShirtObject); // => true
+assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+// eqObjects(shirtObject , longSleeveShirtObject); // => false
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
