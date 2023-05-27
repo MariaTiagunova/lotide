0baf595cda3e7416c48c8d8b8c335346a5cb31d7
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-05-27 17:15:09 -0400

    map.js: linter fixes, takeUntil. js: added tests, linter fixes

--- map.js
+++ map.js
@@ -19,12 +19,12 @@ const assertArraysEqual = (actual, expected) => {
 };
 
 const map = function(array, callback) { // takes an array and a callback function as arguments
-  const results = []; 
+  const results = [];
   for (let item of array) { // iterates over each item in the array and applies the callback function to transform each item
-   results.push(callback(item)); // The transformed items are then collected in a new array
+    results.push(callback(item)); // The transformed items are then collected in a new array
   }
   return results;
-}
+};
 
 const words = ["ground", "control", "to", "major", "tom"];
 
--- takeUntil.js
+++ takeUntil.js
@@ -1,12 +1,32 @@
-const takeUntil = function(array, callback) {
-let result = [];
-for (let i = 0; i < array.length; i++) {
-  if (callback(array[i])) { // Check if the callback returns a truthy value for the current element
-    return result; // If true, return the result array
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
   }
-  result.push(array[i]); // If false, push the element to the result array
-}
-return result;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) { // Check if the callback returns a truthy value for the current element
+      return result; // If true, return the result array
+    }
+    result.push(array[i]); // If false, push the element to the result array
+  }
+  return result;
 };
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
@@ -18,3 +38,6 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
