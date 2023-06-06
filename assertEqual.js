4f50f48bdfc2350a0fd6fde1cfed842a96dcd083
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-06-06 18:08:19 -0400

    Refactored assertEqual and head: module exports, separated tests

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Cat", "Cat");
-assertEqual(5, 7);
+module.exports = assertEqual;
