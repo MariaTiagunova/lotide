d5331b13473ea7178f09fbcbfbf61c3231f180b4
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-06-07 19:36:59 -0400

    Refactored tailTest to use Mocha and Chai, added test for an empty array

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,18 @@
 // test/tailTest.js
-
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]); // Lighthouse, Labs, 2
-const res = tail(["Hello"]);
+describe("#tail", () => {
+  it("returns 'Lighthouse', 'Labs' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
 
-assertEqual(res.length, 0);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result.length, 3); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[0], "LighthouseZ"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+  it("returns [] for ['Lighthouse']", () => {
+    assert.deepEqual(tail(['Lighthouse']), []);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
