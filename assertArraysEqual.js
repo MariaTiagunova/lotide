d61716fa71f48285ec075a6278119e6271642fb4
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-06-08 19:18:43 -0400

    Added README.md, modified packsge.json to fit the npm requirements, revised the assertArraysEqual output messages

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,9 +2,9 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (actual, expected) => {
   if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅ Arrays are equal: ${actual} === ${expected}`);
   } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌ Arrays are not equal: ${actual} !== ${expected}`);
   }
 };
 
