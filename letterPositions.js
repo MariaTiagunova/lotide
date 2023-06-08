0277c71ff4b1497fef9227e5bead1bcf4b1905cd
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-06-08 12:34:15 -0400

    Added module exports to 'letterPositions', tests moved to letterPositionsTest.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,24 +1,3 @@
-const eqArrays = (actual, expected) => {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = (actual, expected) => {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const letterPositions = function(sentence) {
   const result = {};
   // logic to update results here
@@ -33,7 +12,4 @@ const letterPositions = function(sentence) {
   return result;
 };
 
-//Test
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("lighthouse").h, [3, 5]);
+module.exports = letterPositions;
