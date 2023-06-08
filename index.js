f5e742c1d016c0a7087f9aa25f3bec75e43f373b
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-06-08 12:17:26 -0400

    Aded index.js exporting functions

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const without = require('./without');
+const flatten = require('./flatten');
+const countOnly = require('./countOnly');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  assertArraysEqual: assertArraysEqual,
+  without: without,
+  flatten: flatten,
+  countOnly: countOnly
+};
